1bff65933a549ab829e759722b6722601d9f94c3
brtysek <113090740+brtyy@users.noreply.github.com>
2023-09-27 09:46:29 +0200

    Update picoman.js

--- picoman.js
+++ picoman.js
@@ -1,1 +1,10 @@
+ // Funkce pro aktualizaci času
+ function updateTimer() {
+    const timerElement = document.getElementById('timer');
+    let seconds = parseInt(timerElement.textContent);
+    seconds++;
+    timerElement.textContent = seconds;
+}
 
+// Spustit aktualizaci času ihned po načtení stránky
+setInterval(updateTimer, 1000); // Aktualizace každou sekundu
