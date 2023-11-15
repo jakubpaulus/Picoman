132a7807571cc1ad209a425db828ebb9e6173a7f
jakubpaulus <113091265+jakubpaulus@users.noreply.github.com>
2023-11-15 09:22:54 +0100

    Add files via upload

--- docs/script.js
+++ docs/script.js
@@ -5,25 +5,36 @@ window.alert('Zdravím tě ' + jmenoFetaka + '.');
 let mnozstviFetu = window.prompt('Kolik toho u sebe máš?');
 console.log(`Tvé množství fetu je ${mnozstviFetu}.`);
 
-const MINIMUM = 5;
+if (isNaN (mnozstviFetu)) {
+    window.alert(`To není číslo ty pako.`);
+    mnozstviFetu = window.prompt(`Kolik toho teda máš?`);
+}
 
-if (mnozstviFetu === MINIMUM) {
+const MINIMUM = 5;
+const MINIMALNIVEK = 18;
+
+let vekFetaka = window.prompt ('Kolik ti je let feťáku?')
+
+if (isNaN (vekFetaka)) {
+    let vekFetaka = window.prompt('Tohle není číslo, kolik ti je teda let?')
+}
+
+if (Number(mnozstviFetu) === MINIMUM && vekFetaka >= MINIMALNIVEK) {
     window.alert('Vítej mezi námi!');
 }
 
-else if (mnozstviFetu > MINIMUM) {
+else if (mnozstviFetu > MINIMUM && vekFetaka >= MINIMALNIVEK) {
     window.alert('Proč jsi nepřišel dřív?! Dávno bychom tě vzali!');
 }
 
 else {
-    if (!isNaN (mnozstviFetu)) {
-        window.alert(`To je málo, budeš se muset víc snažit!`);
-    }
-    else {
-        window.alert(`To není číslo ty pako.`);
-        mnozstviFetu = window.prompt(`Kolik toho teda máš?`);
+    if (mnozstviFetu < MINIMUM || vekFetaka < MINIMALNIVEK) {
+        window.alert (`Přijď až budeš splňovat podmínky.`);
     }
 }
+
+
+
 
 
 
